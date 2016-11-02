9f955d8c7cda67cb069780e256ab9acf2f8360d6
Taylor Hessel <taylorhessel@gmail.com>
2016-11-02 15:55:34 -0600

    Removed remaining web-app-related code, this is an API

--- app.js
+++ app.js
@@ -11,17 +11,10 @@ var visits = require('./routes/visits');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/visits', visits);
 
@@ -39,7 +32,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.send({
       message: err.message,
       error: err
     });
